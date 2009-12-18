85614a3f6e1f0447d733a06e6c20c82dc532b1cc
unknown <pap@.clients.brainstore.com>
2009-12-18 00:01:36 -0800

    first step map test

--- test/unit/mapping_and_routing/static_map_test.rb
+++ test/unit/mapping_and_routing/static_map_test.rb
@@ -2,4 +2,18 @@ require File.dirname(__FILE__) + '/../../test_helper'
 
 class StaticMapTest < ActiveSupport::TestCase
  
+ def test_basic
+   @lax = Airport.find("lax")
+   @zrh = Airport.find("zrh")
+   
+   @flight_map = FlightMap.new(@lax,@zrh)
+   assert_not_nil(@flight_map)
+   
+   assert_equal(@flight_map.map_width,450)
+   assert_equal(@flight_map.map_height,300)
+   @flight_map.set_size(480,240)
+   assert_equal(@flight_map.map_width,480)
+   assert_equal(@flight_map.map_height,240)
+   
+ end
 end
