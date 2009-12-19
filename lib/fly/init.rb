644800cc86c09d4d316c935159d3981b259ddd7d
unknown <pap@.clients.brainstore.com>
2009-12-19 07:06:25 -0800

    more subclassing

--- lib/fly/init.rb
+++ lib/fly/init.rb
@@ -1,29 +1,31 @@
 $:.unshift "#{File.dirname(__FILE__)}/lib/builders"
-  require "creator"
+  require "creator" #superclass
   require "flight_creator"
   require "plane_creator"
   
-$:.unshift "#{File.dirname(__FILE__)}/lib/mapping_and_routing"
+$:.unshift "#{File.dirname(__FILE__)}/lib/mapping"
+  require "static_map" #superclass
   require "airport_map"
   require "flight_map"
-  require "flight_plan"
-  require "flight_segment"
   require "map_marker"
   require "map_path"
-  require "segment_series"
-  require "static_map"
   
+$:.unshift "#{File.dirname(__FILE__)}/lib/routing"
+  require "flight_plan"
+  require "flight_segment"
+  require "segment_series"
+
 $:.unshift "#{File.dirname(__FILE__)}/lib/other"
-  require "fly_error"
-  require "query_collection"
+  require "query_collection" #superclass
   require "query_collection_exclusive_key"
   require "query_collection_non_exclusive_key"
   require "query_param"
+  require "fly_error"
 
 $:.unshift "#{File.dirname(__FILE__)}/lib/quantities"
+  require "quantity" #superclass
   require "distance_in_miles"
   require "duration_in_seconds"
   require "number_of_passengers"
-  require "quantity"
   require "starting_miles_count"
   require "starting_pax_count"
