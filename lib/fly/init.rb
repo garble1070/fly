8b244ddc914d40a05a976c0426a17412d4c8ef04
unknown <pap@.clients.brainstore.com>
2009-12-19 07:09:01 -0800

    more subclassing

--- lib/fly/init.rb
+++ lib/fly/init.rb
@@ -10,16 +10,7 @@ $:.unshift "#{File.dirname(__FILE__)}/lib/mapping"
   require "map_marker"
   require "map_path"
   
-$:.unshift "#{File.dirname(__FILE__)}/lib/routing"
-  require "flight_plan"
-  require "flight_segment"
-  require "segment_series"
-
 $:.unshift "#{File.dirname(__FILE__)}/lib/other"
-  require "query_collection" #superclass
-  require "query_collection_exclusive_key"
-  require "query_collection_non_exclusive_key"
-  require "query_param"
   require "fly_error"
 
 $:.unshift "#{File.dirname(__FILE__)}/lib/quantities"
@@ -29,3 +20,15 @@ $:.unshift "#{File.dirname(__FILE__)}/lib/quantities"
   require "number_of_passengers"
   require "starting_miles_count"
   require "starting_pax_count"
+  
+$:.unshift "#{File.dirname(__FILE__)}/lib/queries"
+  require "query_collection" #superclass
+  require "query_collection_exclusive_key"
+  require "query_collection_non_exclusive_key"
+  require "query_param"
+  
+$:.unshift "#{File.dirname(__FILE__)}/lib/routing"
+  require "flight_plan"
+  require "flight_segment"
+  require "segment_series"
+
