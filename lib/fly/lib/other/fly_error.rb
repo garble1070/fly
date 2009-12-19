8876ab093358242a538e9a6b5ff419e1e358b7ee
unknown <pap@.clients.brainstore.com>
2009-12-19 06:42:22 -0800

    more map class work

--- lib/fly/lib/other/fly_error.rb
+++ lib/fly/lib/other/fly_error.rb
@@ -1,6 +1,10 @@
 class FlyError
-  #  Raise an error if this method is called.
-  def raise_superclass_error
-    raise RuntimeError.new("This method is meant to be called in the subclass.")
+  
+  class << self
+    #  Raise an error if this method is called.
+    def raise_superclass_error
+      raise RuntimeError.new("This method is meant to be called in the subclass.")
+    end
+    
   end
 end
