ec39aedc174c337da8f2612ec90339b4bb30d702
unknown <pap@.clients.brainstore.com>
2009-12-20 14:33:29 -0800

    started account billing

--- test/unit/mapping/airport_map_test.rb
+++ test/unit/mapping/airport_map_test.rb
@@ -11,7 +11,7 @@ class AirportMapTest < ActiveSupport::TestCase
     
     part1 = "?maptype=terrain&format=jpg&sensor=false&size=450x300"
     part2 = "&key=ABQIAAAAHjDfn4JyllVUPtyJ31qLhhSjmO-kHhwqX2l12pfya7ICKXzFqhRq3QRp0Ql03P59GaKmwMasDuskrA"
-    part3 = "&markers=color:yellow|32.897,-97.038&markers=color:yellow|38.945,-77.456&markers=color:yellow|33.943,-118.408"
+    part3 = "&markers=color:yellow|label:D|32.897,-97.038&markers=color:yellow|label:I|38.945,-77.456&markers=color:yellow|label:L|33.943,-118.408"
     expected_url = part1 + part2 + part3
     assert_equal(@a_map.complete_url,expected_url)
   end
