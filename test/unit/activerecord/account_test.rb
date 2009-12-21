5fd8c91753473caa29af5f6ea57b7a0d06988e6c
unknown <pap@.clients.brainstore.com>
2009-12-20 17:23:03 -0800

    update 5:22pm

--- test/unit/activerecord/account_test.rb
+++ test/unit/activerecord/account_test.rb
@@ -3,12 +3,21 @@ require File.dirname(__FILE__) + '/../../test_helper'
 class AccountTest < ActiveSupport::TestCase
 
  def test_transaction_stuff
+   @account = Account.find(1)
+   assert_equal(300.50,@account.balance)
+   
+   assert_equal(true,@account.debit(100))
+   assert_equal(200.50,@account.balance)
+   
+   assert_equal(false,@account.debit(400))
+   assert_equal(200.50,@account.balance)
+
+   assert_equal(true,@account.credit(100))
+   assert_equal(300.50,@account.balance)
    
  end
  
  
-# debit(amount)
-# credit(amount)
 # balance_adequate_for_transaction?(amount)
 # record_transaction
 
