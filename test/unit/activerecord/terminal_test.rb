6b655544712e3d4db876d42fe136f371d312551d
unknown <pap@.clients.brainstore.com>
2009-12-22 11:51:58 -0800

    added coverage to terminal

--- test/unit/activerecord/terminal_test.rb
+++ test/unit/activerecord/terminal_test.rb
@@ -1,23 +1,32 @@
 require File.dirname(__FILE__) + '/../../test_helper'
 
 class TerminalTest < ActiveSupport::TestCase
- 
+  
   def test_associations_with_airport_class
     load_instance_vars
     
-    assert @terminal_1.airport == @lax
+    assert_equal(@lax,@terminal_1.airport)
   end
   
   def test_associations_with_user_class
     load_instance_vars
     
-    assert @terminal_6.airline == @airline_2
+    assert_equal(@airline_2,@terminal_6.airline)
   end
   
-  def test_airline_stuff
+  def test_owners_flc_account
     load_instance_vars
-#    owners_flc_account
-#    charge_cost_to_owners_account
+    
+    assert_equal(@account_1,@terminal_1.owners_flc_account)
+  end
+  
+  def test_charge_cost_to_owners_account
+    load_instance_vars
+    
+    assert_equal(300.50,@account_1.balance)
+    @terminal_1.charge_cost_to_owners_account
+    load_instance_vars
+    assert_equal(200.50,@account_1.balance)
   end
   
 end
