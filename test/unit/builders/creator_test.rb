e5f1f61ea7c1b24f7d484f45f688e79adf559d58
unknown <pap@.clients.brainstore.com>
2010-01-03 03:26:20 -0800

    abstracted optional params mechanism in creator classes

--- test/unit/builders/creator_test.rb
+++ test/unit/builders/creator_test.rb
@@ -6,7 +6,6 @@ class CreatorTest < ActiveSupport::TestCase
     @creator = Creator.new
     assert_raise(RuntimeError) {@creator.generate_new_item_object}
     assert_raise(RuntimeError) {@creator.insert_required_params_into_new_item_object}
-    assert_raise(RuntimeError) {@creator.insert_optional_params_into_new_item_object}
   end
 end
   
